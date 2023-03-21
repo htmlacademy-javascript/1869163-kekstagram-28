3a963841cddec3320c080f35adeb8235d143d0aa
vhs-cake <vali.kravtsova@gmail.com>
2023-03-21 14:19:52 +0100

    Открывается и закрывается (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,6 +1,14 @@
 import { isEscapeKey } from './util.js';
 import { posts } from './data.js';
-import { createSocialComment } from './social-comments.js';
+import {
+  createSocialComment,
+  currentCommentSection,
+  MAX_NUMBER_OF_COMMENTS,
+  resetCounter,
+  incrementCounterIndex,
+  initCounter,
+  getIsLastSection,
+} from './social-comments.js';
 
 const body = document.body;
 const bigPictureElement = document.querySelector('.big-picture');
@@ -8,8 +16,13 @@ const picturesBlock = document.querySelector('.pictures');
 const bigPictureCloseElement = bigPictureElement.querySelector(
   '.big-picture__cancel'
 );
+const socialCommentCountStartSpan = bigPictureElement.querySelector(
+  '.social__comment-count-start'
+);
 
-function openBigPicture(url, likesCount, commentsCount, description) {
+let post = null;
+
+const openBigPicture = (url, likesCount, commentsCount, description) => {
   bigPictureElement.classList.remove('hidden');
   body.classList.add('modal-open');
 
@@ -23,13 +36,50 @@ function openBigPicture(url, likesCount, commentsCount, description) {
     if (isEscapeKey(evt)) {
       evt.preventDefault();
       bigPictureElement.classList.add('hidden');
+      body.classList.remove('modal-open');
     }
   });
-}
+};
 
-function closeBigPicture() {
+const updateComments = () => {
+  const { url, likes, comments, description } = post;
+  initCounter(comments);
+  openBigPicture(url, likes, comments.length, description);
+
+  const commentsUl = bigPictureElement.querySelector('.social__comments');
+  const commentsLiCollection = post.comments
+    .slice(
+      currentCommentSection * MAX_NUMBER_OF_COMMENTS,
+      (currentCommentSection + 1) * MAX_NUMBER_OF_COMMENTS
+    )
+    .map(({ avatar, name, message }) =>
+      createSocialComment(avatar, name, message)
+    );
+
+  commentsUl.innerHTML = '';
+
+  commentsUl.append(...commentsLiCollection);
+
+  socialCommentCountStartSpan.textContent =
+    (currentCommentSection + 1) * MAX_NUMBER_OF_COMMENTS +
+    (getIsLastSection() ? comments.length % MAX_NUMBER_OF_COMMENTS : 0);
+};
+
+const onNextButtonClick = () => {
+  incrementCounterIndex();
+  updateComments();
+};
+
+const nextButton = bigPictureElement.querySelector('.social__comments-loader');
+nextButton.addEventListener('click', onNextButtonClick);
+
+const closeBigPicture = () => {
   bigPictureElement.classList.add('hidden');
   body.classList.remove('modal-open');
+  nextButton.removeEventListener('click', onNextButtonClick);
+  post = null;
+  resetCounter();
+  socialCommentCountStartSpan.textContent = 5;
 
   document.removeEventListener('keydown', (evt) => {
     if (isEscapeKey(evt)) {
@@ -37,29 +87,15 @@ function closeBigPicture() {
       bigPictureElement.classList.add('hidden');
     }
   });
-}
+};
 
 picturesBlock.addEventListener('click', (evt) => {
   const a = evt.target.closest('a');
   if (!a) {
     return;
   }
-  const post = posts[a.dataset.index];
-  const { url, likes, comments, description } = post;
-  openBigPicture(url, likes, comments.length, description);
-
-  const commentsUl = bigPictureElement.querySelector('.social__comments');
-  const commentsLiCollection = post.comments.map(({ avatar, name, message }) =>
-    createSocialComment(avatar, name, message)
-  );
-  commentsUl.append(...commentsLiCollection);
-
-  const socialCommentCount = bigPictureElement.querySelector(
-    '.social__comment-count'
-  );
-  const commentsLoader = bigPictureElement.querySelector('.comments-loader');
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+  post = posts[a.dataset.index];
+  updateComments(post);
 });
 
 bigPictureCloseElement.addEventListener('click', () => {

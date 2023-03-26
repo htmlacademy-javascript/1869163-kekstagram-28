3e8779183f08844f39c124f5f50ae161321629f8
vhs-cake <vali.kravtsova@gmail.com>
2023-03-27 00:25:15 +0200

    Помощь друга

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import './miniature.js';
 import './big-picture.js';
 import './form.js';
+import './scale-img.js';
+import './fiter-effects.js';
---
+++ js/scale-img.js
@@ -0,0 +1,39 @@
+const SCALE_STEP = 25;
+const DEFAULT_SCALE_VALUE = 100;
+
+let scaleValueNumber = DEFAULT_SCALE_VALUE;
+
+const editorForm = document.querySelector('.img-upload__overlay');
+const imgPreview = editorForm.querySelector('.img-upload__preview img');
+const scaleInput = editorForm.querySelector('.scale__control--value');
+const scaleButtonSmaller = editorForm.querySelector('.scale__control--smaller');
+const scaleButtonBigger = editorForm.querySelector('.scale__control--bigger');
+
+const updateScaleInput = () => (scaleInput.value = `${scaleValueNumber}%`);
+
+updateScaleInput();
+
+const scaleImg = () => {
+  imgPreview.style.transform = `scale(${Number(scaleValueNumber) / 100})`;
+};
+
+const onSmallerButtonClick = () => {
+  if (Number(scaleValueNumber) < SCALE_STEP * 2) {
+    return;
+  }
+  scaleValueNumber = Number(scaleValueNumber) - SCALE_STEP;
+  updateScaleInput();
+  scaleImg();
+};
+
+const onBiggerButtonClick = () => {
+  if (Number(scaleValueNumber) > SCALE_STEP * 3) {
+    return;
+  }
+  scaleValueNumber = Number(scaleValueNumber) + SCALE_STEP;
+  updateScaleInput();
+  scaleImg();
+};
+
+scaleButtonSmaller.addEventListener('click', onSmallerButtonClick);
+scaleButtonBigger.addEventListener('click', onBiggerButtonClick);
